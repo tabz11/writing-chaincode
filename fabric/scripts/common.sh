14c7f0a24715293154ac0c0d956a92d0954972c3
Paul Sitoh <paulwizviz@gmail.com>
2017-12-17 10:52:22 +0000

    Update fabric scripts
    Since only channel name is common, we only place it in the common file

--- fabric/scripts/common.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CHANNELNAME="mychannel"
-CHAINCODEID="mycc"
-
-PATH_TO_CHAINCODE='github.com/hyperledger/fabric/examples/chaincode/go/minimalcc'
-CHAINCODE_VERSION='1.0'
-CHAINCODE_CONSTRUCTOR='{"Args":["methodName","a","100","b","200"]}'
